95967b841333dc98a774761495445974dd8319f7
joooo97 <68749697+joooo97@users.noreply.github.com>
2022-01-01 19:52:38 +0900

    Update Solution.java
    타겟 넘버 문제 풀이 소스코드 수정

--- dfs_bfs/타겟 넘버/Solution.java
+++ dfs_bfs/타겟 넘버/Solution.java
@@ -2,24 +2,24 @@ class Solution {
     public int t, ans;
     public int[] arr_numbers;
     
-	public void recur(int idx, int cur) {
+	public void dfs(int idx, int cur) {
 		if(idx == arr_numbers.length) {
 			if(cur == t) ans += 1;
 			return;
 		}
 		
 		// cur + numbers[idx]
-		recur(idx + 1, cur + arr_numbers[idx]);
+		dfs(idx + 1, cur + arr_numbers[idx]);
 		
 		// cur - numbers[idx]
-		recur(idx + 1, cur - arr_numbers[idx]);
+		dfs(idx + 1, cur - arr_numbers[idx]);
 	}
     
     public int solution(int[] numbers, int target) {
     	arr_numbers = numbers;
     	t = target;  
     	
-    	recur(0, 0);
+    	dfs(0, 0);
     	        
         return ans;
     }
